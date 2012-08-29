2a64a02129f7658cabe516a1708d956f4a7dc559
Harry Edwards <harryedwards@mac.com>
2012-08-29 06:45:01 -0700

    Finished chapter 5

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,40 +2,32 @@ require 'spec_helper'
 
 describe "StaticPages" do
   
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+  
  describe "Home page" do
-      it "should have the content 'Sample App'" do
-        visit '/static_pages/home'
-        page.should have_content('Sample App')
-      end
+      before(:each) {  visit root_path }
       
-      it "should have the right title" do
-        visit '/static_pages/home'
-        page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Home")
-      end
+      it { page.should have_selector('h1', text: 'Sample App')}
+      it { page.should have_selector('title', :text => full_title('')) }
+      
   end
   
   describe "Help page" do
-       it "should have the content 'Help'" do
-         visit '/static_pages/help'
-         page.should have_content('Help')
-       end
-       
-       it "should have the right title" do
-         visit '/static_pages/help'
-         page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Help")
-       end
+    before(:each) { visit help_path }
+       it {  page.should have_content('Help') }
+       it { page.should have_selector('title', :text => full_title('Help')) }
    end
    
    describe "About page" do
-        it "should have the content 'About'" do
-          visit '/static_pages/about'
-          page.should have_content('About')
-        end
-        
-        it "should have the right title" do
-          visit '/static_pages/about'
-          page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | About")
-        end
+     before(:each) { visit about_path }
+        it {  page.should have_content('About')  }
+        it { page.should have_selector('title', :text => full_title('About')) }
     end
+    
+    describe "Contact page" do
+      before(:each) { visit contact_path }
+         it {  page.should have_content('Contact')  }
+         it { page.should have_selector('title', :text => full_title('Contact')) }
+     end
 
 end
